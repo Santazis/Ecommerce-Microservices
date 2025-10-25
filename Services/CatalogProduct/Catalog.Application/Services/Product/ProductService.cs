fedb963025fe4cb4a246078999093905bff765ee
Still <ssantazis907@gmail.com>
2025-10-25 22:22:37 +0400

    refactoring

--- Services/CatalogProduct/Catalog.Application/Services/Product/ProductService.cs
+++ Services/CatalogProduct/Catalog.Application/Services/Product/ProductService.cs
@@ -13,7 +13,9 @@ public class ProductService : IProductService
     private readonly ApplicationDbContext _dbContext;
     private readonly ICatalogService _catalogService;
     private readonly ILogger<ProductService> _logger;
-    public ProductService(ApplicationDbContext dbContext, ICatalogService catalogService, ILogger<ProductService> logger)
+
+    public ProductService(ApplicationDbContext dbContext, ICatalogService catalogService,
+        ILogger<ProductService> logger)
     {
         _dbContext = dbContext;
         _catalogService = catalogService;
@@ -22,20 +24,21 @@ public class ProductService : IProductService
 
     public async Task<Result<Guid>> CreateAsync(CreateProductRequest request, CancellationToken cancellation)
     {
-        _logger.LogInformation("Creating product {name} for catalog {catalogId}",request.Name,request.CatalogId);
+        _logger.LogInformation("Creating product {name} for catalog {catalogId}", request.Name, request.CatalogId);
         var catalogExists = await _catalogService.CatalogExistsAsync(request.CatalogId, cancellation);
         if (catalogExists.IsFailure)
         {
-            _logger.LogError("Catalog {catalogId} not found",request.CatalogId);
+            _logger.LogError("Catalog {catalogId} not found", request.CatalogId);
             return Result<Guid>.Failure(catalogExists.Error);
         }
+
         var amount = Money.Create(request.Price, request.Currency);
         var product = Domain.Products.Product.Create(request.Name, request.Description, amount, request.CatalogId,
             request.StockQuantity,
             request.MerchantId);
-        await _dbContext.Products.AddAsync(product,cancellation);
+        await _dbContext.Products.AddAsync(product, cancellation);
         await _dbContext.SaveChangesAsync(cancellation);
-        _logger.LogInformation("Product created {productId}",product.Id);
+        _logger.LogInformation("Product created {productId}", product.Id);
         return Result<Guid>.Success(product.Id);
     }
 }
